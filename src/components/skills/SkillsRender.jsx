7879d46428c5d7c19332cf78e600c690047a1325
Venkat <gvk496@gmail.com>
2021-12-06 13:40:02 +0530

    Removed MongoDB, Nodejs and Selenium

--- src/components/skills/SkillsRender.jsx
+++ src/components/skills/SkillsRender.jsx
@@ -16,27 +16,34 @@ const SkillsRender = ({ what }) => {
               <div
                 className={`w-72 md:w-80 xl:w-96 shadow rounded ${
                   darkMode ? "bg-pink-100" : "bg-blue-100"
-                } `}>
+                } `}
+              >
                 <div
                   className={`h-2 rounded ${
                     darkMode ? "bg-pink-500" : "bg-blue-400"
                   } transition-all`}
-                  style={{ width: `${level}%` }}></div>
+                  style={{ width: `${level}%` }}
+                ></div>
               </div>
             </div>
           ))
         : technologies.map(({ tech, level }, index) => (
-            <div className="p-2 space-y-1" key={index}>
-              <p>{tech}</p>
-              <div
-                className={`w-72 md:w-80 xl:w-96 shadow rounded ${
-                  darkMode ? "bg-pink-100" : "bg-blue-100"
-                } `}>
+            <div className="mb-4">
+              <div className="p-2 space-y-1" key={index}>
+                <p>{tech}</p>
                 <div
-                  className={`h-2 rounded ${
-                    darkMode ? "bg-pink-500" : "bg-blue-400"
-                  } transition-all`}
-                  style={{ width: `${level}%` }}></div>
+                  className={`w-72 md:w-80 xl:w-96 shadow rounded ${
+                    darkMode ? "bg-pink-100" : "bg-blue-100"
+                  } `}
+                >
+                  <div
+                    className={`h-2 rounded ${
+                      darkMode ? "bg-pink-500" : "bg-blue-400"
+                    } transition-all`}
+                    style={{ width: `${level}%` }}
+                  ></div>
+                  
+                </div>
               </div>
             </div>
           ))}
