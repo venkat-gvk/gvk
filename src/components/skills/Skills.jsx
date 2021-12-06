a30bf67d9391ac310e87d656a5bdd5f081d1c9f0
Venkat <gvk496@gmail.com>
2021-12-06 13:39:52 +0530

    Removed MongoDB, Nodejs and Selenium

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -11,11 +11,13 @@ const Skills = () => {
     <section
       data-aos="zoom-in"
       id="skills"
-      className="p-2 pb-3 -mt-4 sm:mb-16 lg:mb-12 sm:mt-1">
+      className="p-2 pb-3 -mt-4 sm:mb-16 lg:mb-12 sm:mt-1"
+    >
       <h2
         className={`text-3xl uppercase text-center transition-all duration-500 ${
           darkMode ? "text-pink-500" : "text-gray-700"
-        } sm:pb-2 `}>
+        } sm:pb-2 `}
+      >
         Skills
       </h2>
       <div className="grid grid-row-1 md:grid-cols-2 p-1 mt-4 lg:mt-8 sm:mx-auto">
@@ -23,17 +25,19 @@ const Skills = () => {
         <div
           className={`relative flex flex-col justify-center items-center space-y-4 text-left
         ${darkMode ? "text-white" : "text-gray-700"}
-        `}>
+        `}
+        >
           <p className="font-semibold">Programming Languages</p>
           <SkillsRender what="lang" />
         </div>
 
         {/* Tecnhologies */}
         <div
-          className={`flex flex-col justify-center items-center space-y-4 mt-8 sm:mt-0
+          className={`flex flex-col justify-start items-center space-y-4
           text-left
-        ${darkMode ? "text-white" : "text-gray-700"}`}>
-          <p className="font-semibold">Technologies</p>
+        ${darkMode ? "text-white" : "text-gray-700"}`}
+        >
+          <p className="font-semibold mt-4 sm:mt-0">Technologies</p>
           <SkillsRender what="tech" />
         </div>
       </div>
